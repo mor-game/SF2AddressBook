754cc13da623d5a99a3b1b3d253bb419dca91658
mor-game <morgane.hivet@gmail.com>
2014-08-27 12:48:13 +0200

    - modif fichier appKernel - création des Entities

---
+++ src/carnet/AddressBookBundle/Entity/Societe.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace carnet\AddressBookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Societe
+ *
+ * @ORM\Table(name="societe")
+ * @ORM\Entity
+ */
+class Societe
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_societe", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idSociete;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=80, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="text", nullable=false)
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=80, nullable=false)
+     */
+    private $ville;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="cp", type="boolean", nullable=false)
+     */
+    private $cp;
+
+
+}
